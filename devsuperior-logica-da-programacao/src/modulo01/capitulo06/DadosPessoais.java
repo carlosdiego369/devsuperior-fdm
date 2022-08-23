92cba27549b04011ac77887899ec49b5d710aa7c
CARLOS DIEGO DE FREITAS SILVA <77064886+carlosdiego369@users.noreply.github.com>
2022-08-23 10:55:40 -0300

    DadosPessoais

---
+++ devsuperior-logica-da-programacao/src/modulo01/capitulo06/DadosPessoais.java
@@ -0,0 +1,74 @@
+package modulo01.capitulo06;
+
+import java.util.Scanner;
+
+public class DadosPessoais {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		cadastrarDados(input);
+
+		input.close();
+	}
+
+	public static void cadastrarDados(Scanner input) {
+
+		double maiorAltura = 0, menorAltura = maiorAltura, mediaAlturaMulheres = 0;
+		int qtdHomens = 0, qtdMulheres = 0;
+
+		imprimir("Quantas pessoas serao digitadas? ");
+		int num = input.nextInt();
+
+		double[] alturas = new double[num];
+		String[] generos = new String[alturas.length];
+
+		for (int i = 0; i < num; i++) {
+			imprimir("Altura da " + (i + 1) + "a pessoa: ");
+			alturas[i] = input.nextDouble();
+			input.nextLine();
+			boolean validacao = false;
+			while (!validacao) {
+				imprimir("Genero da " + (i + 1) + "a pessoa: ");
+				generos[i] = input.nextLine();
+
+				if (!generos[i].equalsIgnoreCase("F") && !generos[i].equalsIgnoreCase("M")) {
+					msgErro();
+					validacao = false;
+				} else {
+					if (generos[i].equalsIgnoreCase("M")) {
+						qtdHomens++;
+					} else {
+						qtdMulheres++;
+						mediaAlturaMulheres += alturas[i];
+					}
+					validacao = true;
+				}
+			}
+
+			if (alturas[i] > maiorAltura) {
+				maiorAltura = alturas[i];
+				menorAltura = alturas[i];
+			} else {
+				if (alturas[i] > maiorAltura) {
+					menorAltura = alturas[i];
+				}
+			}
+		}
+		mediaAlturaMulheres = (mediaAlturaMulheres / qtdMulheres);
+		imprimir("Menor altura = " + menorAltura);
+		imprimir("\nMaior altura = " + maiorAltura);
+		imprimir("\nMedia das alturas das mulheres = " + mediaAlturaMulheres);
+		imprimir("\nNumero de homens = " + qtdHomens);
+	}
+
+	public static void imprimir(String texto) {
+		System.out.print(texto);
+	}
+
+	public static void msgErro() {
+		System.err.print("Entrada inválida digite novamente!\n");
+	}
+
+}
