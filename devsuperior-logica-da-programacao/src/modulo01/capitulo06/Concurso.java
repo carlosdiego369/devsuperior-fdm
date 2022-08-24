2bff1300bc11557bf27d54cc57ff518b5f1fcd77
CARLOS DIEGO DE FREITAS SILVA <77064886+carlosdiego369@users.noreply.github.com>
2022-08-24 10:54:58 -0300

    Concruso incompleto
    Maquina da empresa

--- devsuperior-logica-da-programacao/src/modulo01/capitulo06/Concurso.java
+++ devsuperior-logica-da-programacao/src/modulo01/capitulo06/Concurso.java
@@ -1,10 +1,44 @@
 package modulo01.capitulo06;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Concurso {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		verificadorDeDadosConcurso(input);
+		
+		input.close();
 	}
 
+	public static void verificadorDeDadosConcurso(Scanner input) {
+		imprimir("Qual a quantidade pessoas? ");
+		int qtdPessoas = input.nextInt();
+		input.nextLine();
+		
+		String [] nomes = new String[qtdPessoas];
+		double[][] notas = new double[2][];
+		
+		for(int i = 0; i < nomes.length;i++) {
+			imprimir("Digite os dados da " + (i + 1) + "a pessoa: ");
+			nomes[i] = input.nextLine();
+			
+			for(int j = 0; j < notas.length; j++) {
+				
+			}
+		}
+		
+	}
+	
+	public static void imprimir(String texto) {
+		System.out.print(texto);
+	}
+	
+	public static double conversao(double num) {
+		DecimalFormat df = new DecimalFormat("#,##0.00");
+		imprimir(df.format(num));
+		return num;
+	}
 }
