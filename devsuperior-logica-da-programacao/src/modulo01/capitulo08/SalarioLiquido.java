fb8fbe9e82941fb45bba231c18eeca978e90abd9
CARLOS DIEGO DE FREITAS SILVA <77064886+carlosdiego369@users.noreply.github.com>
2022-09-08 10:56:54 -0300

    Ultimo exercicio cap08 incompleto

--- devsuperior-logica-da-programacao/src/modulo01/capitulo08/SalarioLiquido.java
+++ devsuperior-logica-da-programacao/src/modulo01/capitulo08/SalarioLiquido.java
@@ -1,10 +1,58 @@
 package modulo01.capitulo08;
 
+import java.util.Scanner;
+
 public class SalarioLiquido {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+
+		calculoSalarioLiquido(input);
+
+		input.close();
 
 	}
 
+	public static void calculoSalarioLiquido(Scanner input) {
+		imprimir("Digite o valor do salario bruto: ");
+		double salarioBruto = input.nextDouble();
+		
+		double salarioLiquido = salarioBruto;
+		salarioLiquido = calculoImposto(salarioBruto);
+		salarioLiquido = calculoPrevidencia(salarioBruto);
+
+		imprimir("Salario liquido = R$ " + salarioLiquido);
+
+	}
+
+	public static double calculoImposto(double salarioBruto) {
+		double imposto;
+		if (salarioBruto < 4000) {
+			imposto = salarioBruto * 20 / 100;
+			salarioBruto = salarioBruto - imposto;
+		} else {
+			imposto = salarioBruto * 25 / 100;
+			salarioBruto = salarioBruto - imposto;
+		}
+
+		return salarioBruto;
+	}
+
+	public static double calculoPrevidencia(double salarioBruto) {
+		double previdencia;
+		if (salarioBruto < 1500) {
+			previdencia = salarioBruto * 10 / 100;
+			salarioBruto = salarioBruto - previdencia;
+		} else {
+			previdencia = salarioBruto * 15 / 100;
+			salarioBruto = salarioBruto - previdencia;
+		}
+
+		return salarioBruto;
+	}
+
+	public static void imprimir(String texto) {
+		System.out.print(texto);
+	}
 }
